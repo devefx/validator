a772b0221d4681004dc310d6c0894addbd278f6e
devefx <devefx@163.com>
2015-10-13 17:29:43 +0800

    Struts2示例项目

--- validator-struts-demo/src/main/java/org/devefx/struts/action/TestAction.java
+++ validator-struts-demo/src/main/java/org/devefx/struts/action/TestAction.java
@@ -1,7 +1,7 @@
 package org.devefx.struts.action;
 
 import org.devefx.struts.validator.SaveValidator;
-import org.devefx.validator.annotation.RequstValidator;
+import org.devefx.validator.annotation.RequestValidator;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -9,8 +9,8 @@ public class TestAction extends ActionSupport {
 	
 	private static final long serialVersionUID = -6739593701085960358L;
 	
-	@RequstValidator(SaveValidator.class)
+	@RequestValidator(SaveValidator.class)
 	public void doSave() {
-		System.out.println("ÑéÖ¤³É¹¦£¬´¦ÀíÇëÇó");
+		System.out.println("éªŒè¯æˆåŠŸï¼Œå¤„ç†è¯·æ±‚");
 	}
 }
