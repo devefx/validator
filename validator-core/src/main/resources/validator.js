fb4df471db65a0818dcd8b4f3141d5b4df2d81e4
devefx <devefx@163.com>
2015-11-05 10:43:05 +0800

    前端增加对支持validatorDate的支持

--- validator-core/src/main/resources/validator.js
+++ validator-core/src/main/resources/validator.js
@@ -1,3 +1,36 @@
+function SimpleDateFormat(pattern) {
+	var object = new Object();
+	object.pattern = pattern;
+	object.regex = new RegExp("^" + pattern.replace("yyyy", "\\d{4}").replace("MM", "(0\\d|1[12])").replace("dd", "([0-2]\\d|3[0-1])")
+		.replace("HH", "([0-1]\\d|2[0-3])").replace("hh", "(0\\d|1[0-2])").replace("mm", "[0-5]\\d").replace("ss", "[0-5]\\d") + "$");
+	object.position = {
+		year: pattern.indexOf("yyyy"), month: pattern.indexOf("MM"), day: pattern.indexOf("dd"),
+		hour: pattern.toLowerCase().indexOf("hh"), minute: pattern.indexOf("mm"), second: pattern.indexOf("ss")
+	};
+	object.parse = function(source) {
+		if(!this.regex.test(source))
+			throw new Error("Unparseable date: \"" + source + "\"");
+		var time = {
+			year: source.substr(this.position.year, 4), month: source.substr(this.position.month, 2),
+			day: source.substr(this.position.day, 2), hour: source.substr(this.position.hour, 2),
+			minute: source.substr(this.position.minute, 2), second: source.substr(this.position.second, 2)
+		};
+		var day31 = "01,03,05,07,08,10,12";
+		if (time.day == 31 && day31.indexOf(time.month) == -1)
+			throw new Error("Unparseable date: \"" + source + "\"");
+		if (time.month == 2 && time.day == 29 && !(time.year % 4 == 0 && time.year % 100 != 0)
+			&& !(time.year % 100 == 0 && time.year % 400 == 0)) {
+			throw new Error("Unparseable date: \"" + source + "\"");
+		}
+		var date = new Date();
+		date.setFullYear(time.year, time.month - 1, time.day);
+		date.setHours(time.hour);
+		date.setMinutes(time.minute);
+		date.setSeconds(time.second);
+		return date;
+	};
+	return object;
+}
 function Validator(selector, handlerError) {
 	var valid = new Object();
 	valid.form = $(selector);
@@ -94,10 +127,26 @@ function Validator(selector, handlerError) {
 			this.addError(errorKey, errorMessage);
 			return ;
 		}
-		
+		try {
+			new SimpleDateFormat(this.getDatePattern()).parse(value);
+		} catch (e) {
+			this.addError(errorKey, errorMessage);
+		}
 	};
 	valid.validateDate = function(field, min, max, errorKey, errorMessage) {
-		
+		var value = this.getParameter(field);
+		if (value == null || "" == value.replace(/\s/ig, "")) {
+			this.addError(errorKey, errorMessage);
+			return ;
+		}
+		try {
+			var format = new SimpleDateFormat(this.getDatePattern());
+			var time = format.parse(value).getTime();
+			if (time < format.parse(min).getTime() || time > format.parse(max).getTime())
+				this.addError(errorKey, errorMessage);
+		} catch (e) {
+			this.addError(errorKey, errorMessage);
+		}
 	};
 	valid.validateEqualField = function(field_1, field_2, errorKey, errorMessage) {
 		var value_1 = this.getParameter(field_1);
