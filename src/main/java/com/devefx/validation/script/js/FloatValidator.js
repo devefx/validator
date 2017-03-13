70ac684323d54d7836fab876ce3a9be097db72f6
devefx <devefx@163.com>
2017-03-13 17:33:44 +0800

    由于js提供的parse函数对数值转换高容错性，导致非数值也能通过验证

--- src/main/java/com/devefx/validation/script/js/FloatValidator.js
+++ src/main/java/com/devefx/validation/script/js/FloatValidator.js
@@ -3,7 +3,7 @@ function FloatValidator(field, errorCode, errorMessage) {
     this.isValid = function (request) {
         var value = request.getParameter(field);
         if (!isEmpty(value)) {
-            return !isNaN(parseFloat(value));
+            return /^-?\d+(\.\d+)?$/.test(value);
         }
         return true;
     };
--- src/main/java/com/devefx/validation/script/js/IntegerValidator.js
+++ src/main/java/com/devefx/validation/script/js/IntegerValidator.js
@@ -3,7 +3,7 @@ function IntegerValidator(field, errorCode, errorMessage) {
     this.isValid = function (request) {
         var value = request.getParameter(field);
         if (!isEmpty(value)) {
-            return !isNaN(parseInt(value));
+            return /^-?\d+$/.test(value);
         }
         return true;
     };
