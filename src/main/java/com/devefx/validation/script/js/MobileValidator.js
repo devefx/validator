ede789025c503ef3a2d8978c8f4431ed8f631b1b
devefx <devefx@163.com>
2017-03-29 16:48:30 +0800

    修复173开头手机号无法通过验证的错误

--- src/main/java/com/devefx/validation/script/js/MobileValidator.js
+++ src/main/java/com/devefx/validation/script/js/MobileValidator.js
@@ -3,7 +3,7 @@ function MobileValidator(field, errorCode, errorMessage) {
     this.isValid = function (request) {
         var value = request.getParameter(field);
         if (!isEmpty(value)) {
-            return /^(0|86|17951)?(13[0-9]|15[012356789]|17[678]|18[0-9]|14[57])[0-9]{8}$/.test(value);
+            return /^(0|86|17951)?(13[0-9]|15[012356789]|17[3678]|18[0-9]|14[57])[0-9]{8}$/.test(value);
         }
         return true;
     };
